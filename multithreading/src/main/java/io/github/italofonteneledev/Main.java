cc9082ea3dba527bdd54ad75fd8e7cc374e04186
Italo Miranda <contadoitalo911@gmail.com>
2026-07-16 18:28:18 -0300

    feat: initial thread code

--- multithreading/src/main/java/io/github/italofonteneledev/Main.java
+++ multithreading/src/main/java/io/github/italofonteneledev/Main.java
@@ -5,12 +5,15 @@ package io.github.italofonteneledev;
 public class Main {
     static void main() throws InterruptedException {
         Thread thread = new Thread(() -> {
-
+            System.out.println("We are now in thread " + Thread.currentThread().getName());
+            System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
         });
+        thread.setName("New Worker Thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
 
-        System.out.println("We are in thread: " + Thread.currentThread().getName() + "before starting a new thread");
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
         thread.start();
-        System.out.println("We are in thread: " + Thread.currentThread().getName() + "after starting a new thread");
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
 
         Thread.sleep(10000);
     }
