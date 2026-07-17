91f935dd1668c08ebedad1ea5b28a1b48fd3d73e
Italo Miranda <contadoitalo911@gmail.com>
2026-07-17 17:37:36 -0300

    feat: interrupt first touch

--- multithreading/src/main/java/io/github/italofonteneledev/Main.java
+++ multithreading/src/main/java/io/github/italofonteneledev/Main.java
@@ -1,116 +1,22 @@
 package io.github.italofonteneledev;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-
 public class Main {
-    public static final int MAX_PASSWORD = 9999;
-    static void main() throws InterruptedException {
-        Random random = new Random();
+    static void main() {
+        Thread thread = new Thread(new BlockingTask());
 
-        Vault vault = new Vault(random.nextInt(MAX_PASSWORD));
+        thread.start();
 
-        System.out.println("Senha do cofre: "+ vault.password);
-
-        List<Thread> threads = new ArrayList<>();
-
-        threads.add(new AscendingHackerThread(vault));
-        threads.add(new DescendingHackerThread(vault));
-        threads.add(new PoliceThread());
-
-        for (Thread thread : threads) {
-            thread.start();
-        }
-
+        thread.interrupt();
     }
 
-    private static class Vault {
-        private int password;
-
-        public Vault(int password) {
-            this.password = password;
-        }
-
-        public boolean isCorrectPassword(int guess) {
+    static class BlockingTask implements Runnable {
+        @Override
+        public void run() {
             try {
-                Thread.sleep(5);
+                Thread.sleep(500000);
             } catch (InterruptedException e) {
+                System.out.println("Exiting blocking thread");
             }
-            return this.password == guess;
-        }
-    }
-
-    private static abstract class HackerThread extends Thread {
-        protected Vault vault;
-
-        public HackerThread(Vault vault) {
-            this.vault = vault;
-            this.setName(this.getClass().getSimpleName());
-            this.setPriority(Thread.MAX_PRIORITY);
-        }
-
-        @Override
-        public void start() {
-            System.out.println("Starting thread: " + this.getName());
-            super.start();
-        }
-    }
-
-    private static class AscendingHackerThread extends HackerThread {
-
-        public AscendingHackerThread(Vault vault) {
-            super(vault);
-        }
-
-        @Override
-        public void run() {
-
-            for(int guess = 0; guess < MAX_PASSWORD; guess++) {
-                if (vault.isCorrectPassword(guess)) {
-                    System.out.println(this.getName() + " guessed the password " + guess);
-                    System.exit(0);
-                }
-            }
-
-        }
-    }
-
-    private static class DescendingHackerThread extends HackerThread {
-
-        public DescendingHackerThread(Vault vault) {
-            super(vault);
-        }
-
-        @Override
-        public void run() {
-            for(int guess = MAX_PASSWORD; guess >= 0; guess--) {
-                if (vault.isCorrectPassword(guess)) {
-                    System.out.println(this.getName() + " guessed the password " + guess);
-                    System.exit(0);
-                }
-            }
-        }
-    }
-
-    private static class PoliceThread extends Thread {
-
-        public PoliceThread() {
-            this.setName(this.getClass().getSimpleName());
-        }
-
-        @Override
-        public void run() {
-            for(int i = 10; i > 0; i--) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                }
-                System.out.println(i);
-            }
-            System.out.println("Game Over for you hackers");
-            System.exit(0);
         }
     }
 }
