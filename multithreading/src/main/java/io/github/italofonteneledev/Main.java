0054692b5393f423fe385ac3db8fd3edc55b011d
Italo Miranda <contadoitalo911@gmail.com>
2026-07-17 18:08:51 -0300

    race condition factorial

--- multithreading/src/main/java/io/github/italofonteneledev/Main.java
+++ multithreading/src/main/java/io/github/italofonteneledev/Main.java
@@ -1,22 +1,66 @@
 package io.github.italofonteneledev;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     static void main() {
-        Thread thread = new Thread(new BlockingTask());
+        List<Long> inputNumbers = Arrays.asList(0L, 3435L, 35435L, 2324L, 4656L, 23L, 2435L, 5566L);
 
-        thread.start();
+        List<FactorialThread> threads = new ArrayList<>();
 
-        thread.interrupt();
+        for(long inputNumber: inputNumbers) {
+            threads.add(new FactorialThread(inputNumber));
+        }
+
+        for(Thread thread : threads) {
+            thread.start();
+        }
+
+        for(int i = 0; i < inputNumbers.size(); i++) {
+            FactorialThread factorialThread = threads.get(i);
+            if(factorialThread.isFinished()){
+                System.out.println("Factorial of " + inputNumbers.get(i) + " is " + factorialThread.getResult());
+            } else {
+                System.out.println("he calculation for " + inputNumbers.get(i) + "is still in progress");
+            }
+        }
+
     }
 
-    static class BlockingTask implements Runnable {
+    public static class FactorialThread extends Thread {
+        private long inputNumber;
+        private BigInteger result = BigInteger.ZERO;
+        private boolean isFinished = false;
+
+        public FactorialThread(long inputNumber) {
+            this.inputNumber = inputNumber;
+        }
+
         @Override
         public void run() {
-            try {
-                Thread.sleep(500000);
-            } catch (InterruptedException e) {
-                System.out.println("Exiting blocking thread");
+            this.result = factorial(inputNumber);
+            this.isFinished = true;
+        }
+
+        public BigInteger factorial(long n) {
+            BigInteger tempResult = BigInteger.ONE;
+
+            for (long i = n; i > 0; i--) {
+                tempResult = tempResult.multiply(new BigInteger(Long.toString(i)));
             }
+
+            return tempResult;
+        }
+
+        public boolean isFinished() {
+            return isFinished;
+        }
+
+        public BigInteger getResult() {
+            return result;
         }
     }
 }
